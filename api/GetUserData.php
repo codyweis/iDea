a50e929a50bb05b967c389dce0290ff2fbd80940
codyweis <codyweisenberger@gmail.com>
2016-04-05 12:19:41 -0600

    Displays profile

--- api/GetUserData.php
+++ api/GetUserData.php
@@ -1,13 +1,18 @@
 <?php
+
 session_start();
+$sessionid = $_GET['PHPSESSID'];
 	if($_SERVER['REQUEST_METHOD'] == 'GET'){
-		
-		$username = $_SESSION['username'];
-		
-		$sql = "SELECT * FROM USER WHERE username = '$username'";
-		
+                $sqlSess = "SELECT user FROM SESSION WHERE session_id = '$sessionid'";
 		require_once('connect.inc.php');
-		
+
+                $runSess = mysqli_query($conn, $sqlSess);
+
+                while($sesRow = mysqli_fetch_array($runSess)){
+                        $sessUser = $sesRow['user'];
+                }
+
+                $sql = "SELECT * FROM USER WHERE username =  '$sessUser'";
 		$run = mysqli_query($conn, $sql);
 		$result = array();
 		
@@ -21,8 +26,8 @@ session_start();
 			));
 		}
 		
-		echo json_encode(array('result'=>$result));
+		echo json_encode(array('resultUser'=>$result));
 
 		mysqli_close($conn);
 	}
-?>
+?>	
